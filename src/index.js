39937b7b81cf9cf8266b4aa8aa822d100a528680
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-11-18 09:59:20 -0800

    created select field and added event listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyService from './js/currency-service';
+function getAPIData(data){
+  
+}
+
+function handleSubmission(e){
+  e.preventDefault();
+
+}
+
+window.addEventListener("load", function() {
+  this.document.getElementById("currency-form").addEventListener('submit', handleSubmission);
+});
