8e8743227ff93eb12f4d997d5b3448e8eb9a629f
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-11-18 11:38:31 -0800

    added api functionality to dynamically print rates

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from './js/currency-service';
-function getAPIData(data){
-  
+function getAPIData(){
+  CurrencyService.getCurrency()
+  .then(response => {
+    if(response instanceof Error) {
+      const errorMsg = `There was a problem accessing the data from Currency Exchange API: ${response.message}`;
+      throw new Error(errorMsg);
+    }
+    printConversion(response);
+  });
+  //.catch(erorr => printError(error));
+}
+function printConversion(data) {
+  console.log(data.conversion_rates);
+  let currency = document.getElementById("currency").value;
+  document.getElementById("response").innerHTML = data.conversion_rates[currency];
 }
 
 function handleSubmission(e){
   e.preventDefault();
+  getAPIData();
 
 }
 
