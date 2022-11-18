318435795ab64f334b80212e6489cea765e0d9c0
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-11-18 10:43:08 -0800

    created dynamic currency drop down

--- src/index.js
+++ src/index.js
@@ -12,5 +12,12 @@ function handleSubmission(e){
 }
 
 window.addEventListener("load", function() {
+  let currencies = ['EUR', 'JPY', 'GBP', 'CNY', 'AUD'];
+  currencies.forEach(function(element) {
+    const option = document.createElement('option');
+    option.value = element;
+    option.innerText = element;
+    document.getElementById('currency').append(option);
+  });
   this.document.getElementById("currency-form").addEventListener('submit', handleSubmission);
 });
