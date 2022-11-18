c6a25ef534d0413991799505b6bfba7749662fff
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-11-18 15:48:24 -0800

    added conversion to multiple currencies

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from './js/currency-service';
-function getAPIData(){
-  CurrencyService.getCurrency()
+function getAPIData(currency){
+  CurrencyService.getCurrency(currency)
   .then(response => {
     if(response instanceof Error) {
       const errorMsg = `There was a problem accessing the data from Currency Exchange API: ${response.message}`;
@@ -20,6 +20,7 @@ function printError(error) {
 
 function printConversion(data) {
   console.log(data.conversion_rates);
+  let currencyFrom = document.getElementById("currency-from").value;
   let currency = document.getElementById("currency").value;
   let amount = parseInt(document.getElementById("amount").value);
   let convertedAmount = (amount * data.conversion_rates[currency]).toFixed(2);
@@ -29,7 +30,7 @@ function printConversion(data) {
     document.getElementById("response").append(h6);
   } else {
     const h6 = document.createElement("h6")
-    h6.append(`${amount} USD is ${convertedAmount} ${currency}`);
+    h6.append(`${amount} ${currencyFrom} is ${convertedAmount} ${currency}`);
     document.getElementById("response").append(h6);
   }
 
@@ -39,7 +40,8 @@ function printConversion(data) {
 function handleSubmission(e){
   e.preventDefault();
   document.getElementById("response").innerText = null;
-  getAPIData();
+  let currency = document.getElementById("currency-from").value;
+  getAPIData(currency);
 
 }
 
@@ -54,7 +56,7 @@ function addToDropDown(currencies, dropDownID){
 }
 
 window.addEventListener("load", function() {
-  let currencies = ['EUR', 'JPY', 'GBP', 'CNY', 'AUD', 'AAA'];
+  let currencies = ['USD', 'EUR', 'JPY', 'GBP', 'CNY', 'AUD', 'AAA'];
   addToDropDown(currencies, "currency-from");
   addToDropDown(currencies, "currency");
   this.document.getElementById("currency-form").addEventListener('submit', handleSubmission);
--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -1,6 +1,6 @@
 export default class CurrencyService {
-  static async getCurrency(){
-    let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.EXCHANGER_KEY}/latest/USD`);
+  static async getCurrency(currency){
+    let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.EXCHANGER_KEY}/latest/${currency}`);
     let responseJson = await response.json();
 
     if(!response.ok){
